dfcd43fd38bc1136696120e3d4f23b52034f8340
高岡智志 <satoshitakaoka0922@gmail.com>
2020-12-09 02:54:26 +0900

    fix:category.css      detail.css      product_detail       js      編集

--- js/script.js
+++ js/script.js
@@ -9,3 +9,22 @@ $(function () {
     $('body,html').toggleClass('scroll');
   });
 });
+
+//商品詳細のサムネイルクリック
+
+$(function () {
+  $(".img_click").on("click", function () {
+    // クリックされたimageのsrc要素を取得
+    var clickIndex = $(this).attr("src");
+
+    // .bigimageが画像を格納する箱
+    $(".big_image")
+      // 箱（.big_image）の中身を全部消す
+      .empty()
+      // クリックされたsrcを持つimgタグを追加
+      // 要素そのものにアニメーションcss用クラス（.opa）を一緒に付与してあげることで
+      // クリックされる度にアニメーション（.opa）が実行される
+      .append('<img src="' + clickIndex + '" class="opa"></img>');
+  });
+});
+
