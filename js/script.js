350df7f583e5e80b6fe4ee8e3cb3f5fd070758bc
高岡智志 <satoshitakaoka0922@gmail.com>
2020-12-09 22:33:54 +0900

    fix:script.js 編集

--- js/script.js
+++ js/script.js
@@ -28,3 +28,40 @@ $(function () {
   });
 });
 
+//インスタグラムAPI取得
+
+$(function () {
+            $.ajax({
+                type: 'GET',
+                url: 'https://graph.facebook.com/v5.0/17841444856097383?fields=name%2Cmedia.limit(4)%7Bcaption%2Clike_count%2Cmedia_url%2Cpermalink%2Cthumbnail_url%7D&access_token=EAAK3kzI5PLYBANeds04LHrctTwunnQ7rezcvnAnXJPpwX2ZCSL0bAGuxUsvbmGyA5auUT9zXlx4Hxb7oewjDK5cq4FtXLoBxAkF0FIYF45R6wcMuSXTL7YZBYYpEe1xmYNhrC38SwKZBqmNZCyD1GJZASpS84lqAQUfRmYh14QjEVPOGO2xs7L91L8iL7V20ZD',
+                dataType: 'json',
+                success: function (json) {
+                    var html = '';
+                    var insta = json.media.data;
+                    for (var i = 0; i < insta.length; i++) {
+                        html += '<div class="instagram__test"><a href="' + insta[i].permalink + '" target="_blank"><img class="instagram__pic" src="' + insta[i].media_url + '"></a></div>';
+                    }
+                    $("#aa").append(html);
+                },
+                error: function () {
+
+                    //エラー時の処理
+                }
+            });
+        });	
+
+
+ //ふわっと出てくるアニメーション       
+
+const targetElement = document.querySelectorAll(".animationTarget");
+
+document.addEventListener("scroll", function () {
+  for (let i = 0; i < targetElement.length; i++) {
+    const getElementDistance =
+      targetElement[i].getBoundingClientRect().top +
+      targetElement[i].clientHeight * 0.8;
+    if (window.innerHeight > getElementDistance) {
+      targetElement[i].classList.add("show");
+    }
+  }
+});
